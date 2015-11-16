128fcbafb5712f04a8c6b76ca7e97b62a4816aa2
YooJ <yoojinkimhong@gmail.com>
2015-11-15 20:56:49 -0800

    clearing-browser checkpoint

--- BlocBrowser/BlocBrowser/ViewController.h
+++ BlocBrowser/BlocBrowser/ViewController.h
@@ -10,6 +10,12 @@
 
 @interface ViewController : UIViewController
 
+/**
+ Replaces the web view with a fresh one, erasing all history. Also updates the URL field and toolbar buttons appropriately.
+ */
+
+- (void) resetWebView;
+
 
 @end
 
--- BlocBrowser/BlocBrowser/ViewController.m
+++ BlocBrowser/BlocBrowser/ViewController.m
@@ -191,5 +191,38 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (void) resetWebView {
+    [self.webView removeFromSuperview];
+    
+    WKWebView *newWebView = [[WKWebView alloc] init];
+    newWebView.navigationDelegate = self;
+    [self.view addSubview:newWebView];
+    
+    self.webView = newWebView;
+    
+    [self addButtonTargets];
+    
+    self.textField.text = nil;
+    [self updateButtonsAndTitle];
+}
+
+- (void) addButtonTargets {
+    for (UIButton *button in @[self.backButton, self.forwardButton, self.stopButton, self.reloadButton]) {
+        [button removeTarget:nil action:NULL forControlEvents:UIControlEventTouchUpInside];
+    }
+    
+    //[self.backButton addTarget:self.webView action:@selector(goBack) forControlEvents:UIControlEventTouchUpInside];
+     //[self.forwardButton addTarget:self.webView action:@selector(goForward) forControlEvents:UIControlEventTouchUpInside];
+     //[self.stopButton addTarget:self.webView action:@selector(stopLoading) forControlEvents:UIControlEventTouchUpInside];
+     //[self.reloadButton addTarget:self.webView action:@selector(reload) forControlEvents:UIControlEventTouchUpInside];
+    [self.backButton setTitle:NSLocalizedString(@"Back", @"Back comnmand") forState:UIControlStateNormal];
+    [self.forwardButton setTitle:NSLocalizedString(@"Forward", @"Forward comnmand") forState:UIControlStateNormal];
+    [self.stopButton setTitle:NSLocalizedString(@"Stop", @"Stop comnmand") forState:UIControlStateNormal];
+    [self.reloadButton setTitle:NSLocalizedString(@"Refresh", @"Reload comnmand") forState:UIControlStateNormal];
+    
+     self.reloadButton.enabled = !self.webView.isLoading && self.webView.URL;
+    
+}
+
 
 @end
