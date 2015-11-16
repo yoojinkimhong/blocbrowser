a6f38d790f1e18b9cfccca76262b0fbf879394f2
YooJ <yoojinkimhong@gmail.com>
2015-11-15 20:00:49 -0800

    work in progress

--- BlocBrowser/BlocBrowser/ViewController.m
+++ BlocBrowser/BlocBrowser/ViewController.m
@@ -36,7 +36,7 @@
     self.textField.returnKeyType = UIReturnKeyDone;
     self.textField.autocapitalizationType = UITextAutocapitalizationTypeNone;
     self.textField.autocorrectionType = UITextAutocorrectionTypeNo;
-    self.textField.placeholder = NSLocalizedString(@"Website URL", @"Placeholder text for web browser URL field");
+    self.textField.placeholder = NSLocalizedString(@"Website URL or Type in your search word", @"Placeholder text for web browser URL field");
     self.textField.backgroundColor = [UIColor colorWithWhite:220/255.0f alpha:1];
     self.textField.delegate = self;
     
@@ -108,6 +108,11 @@
     
     NSString *URLString = textField.text;
     
+    if ([URLString containsString:@" "]) {
+        NSString *escapedString = [URLString stringByReplacingOccurrencesOfString:@" " withString:@"+"];
+        URLString = [NSString stringWithFormat:@"http://www.google.com/search?q=%@", escapedString];
+    }
+
     NSURL *URL = [NSURL URLWithString:URLString];
     
     if (!URL.scheme) {
